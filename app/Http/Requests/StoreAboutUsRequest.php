e8ff176ec0906b447f7d910c3256947b971beeca
Ha Eun Lee <haeunictsis@gmail.com>
2024-06-05 15:34:55 +0700

    ..

--- app/Http/Requests/StoreAboutUsRequest.php
+++ app/Http/Requests/StoreAboutUsRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class StoreAboutUsRequest extends FormRequest
 {
@@ -25,5 +27,14 @@ class StoreAboutUsRequest extends FormRequest
             'image' => 'required',
             'description' => 'required',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'message' => 'Validation error',
+            'errors' => $validator->errors()
+        ], 422));
     }
 }
