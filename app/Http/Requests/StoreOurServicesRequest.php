3acd8db6f88df42ca561131645bcd09c32dbc56f
Ha Eun Lee <haeunictsis@gmail.com>
2024-06-06 15:08:45 +0700

    false->true

--- app/Http/Requests/StoreOurServicesRequest.php
+++ app/Http/Requests/StoreOurServicesRequest.php
@@ -13,7 +13,7 @@ class StoreOurServicesRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/UpdateOurServicesRequest.php
+++ app/Http/Requests/UpdateOurServicesRequest.php
@@ -13,7 +13,7 @@ class UpdateOurServicesRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
