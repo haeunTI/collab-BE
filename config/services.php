b52537012ec6c154b26362735336f3be3a164208
Ha Eun Lee <haeunictsis@gmail.com>
2024-06-07 10:43:39 +0700

    ,

--- config/services.php
+++ config/services.php
@@ -40,7 +40,6 @@ return [
         'client_secret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
         'refresh_token' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
         'folder_id' => env('GOOGLE_DRIVE_FOLDER_ID'),
-        'about_us_folder_id' => env('GOOGLE_DRIVE_ABOUT_US_FOLDER_ID'),
         'social_media_folder_id' => env('GOOGLE_DRIVE_SOCIAL_MEDIA_FOLDER_ID'),
         'banner_folder_id' => env('GOOGLE_DRIVE_BANNER_FOLDER_ID'),
         'testimony_folder_id' => env('GOOGLE_DRIVE_TESTIMONY_FOLDER_ID'),
